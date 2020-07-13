e92f47eb79a9adbe8b83a600d76fdb994f2f5ed7
Stefany Souza <stefanysouza@MacBook-Air-de-Stefany.local>
2020-07-13 16:58:12 -0300

    SOAP demostratuin with Spring Boot

---
+++ soap-service-demo/src/main/java/br/com/soap/service/demo/SoapServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.soap.service.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceDemoApplication.class, args);
+	}
+
+}
---
+++ soap-service-demo/src/main/java/com/demo/soap/students/GetStudentDetailsResponse.java
@@ -0,0 +1,71 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2020.07.07 às 03:47:55 PM BRT 
+//
+
+
+package com.demo.soap.students;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de anonymous complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="StudentDetails" type="{http://soap.demo.com/students}StudentDetails"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "studentDetails"
+})
+@XmlRootElement(name = "GetStudentDetailsResponse")
+public class GetStudentDetailsResponse {
+
+    @XmlElement(name = "StudentDetails", required = true)
+    protected StudentDetails studentDetails;
+
+    /**
+     * Obtém o valor da propriedade studentDetails.
+     * 
+     * @return
+     *     possible object is
+     *     {@link StudentDetails }
+     *     
+     */
+    public StudentDetails getStudentDetails() {
+        return studentDetails;
+    }
+
+    /**
+     * Define o valor da propriedade studentDetails.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link StudentDetails }
+     *     
+     */
+    public void setStudentDetails(StudentDetails value) {
+        this.studentDetails = value;
+    }
+
+}
